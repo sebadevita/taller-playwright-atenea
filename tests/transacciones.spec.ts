6f8edb68633d6b2527f52fe11a850a2935c0ac41
sebadevita <sebassdevita@gmail.com>
2025-12-01 22:27:55 -0300

    prueba cicd

--- tests/transacciones.spec.ts
+++ tests/transacciones.spec.ts
@@ -24,7 +24,6 @@ const testUsuarioReceptor = test.extend({
 testUsuarioEmisor('Verificar transacción exitosa', async ({ page }) => {
   await expect(dashboardPage.dashboardTitle).toBeVisible()
   await dashboardPage.botonEnviarDinero.click()
-  console.log('Seba capos')
   await modalEnviarTransferencia.completarFormulario(TestData.usuarioValido.email, '250')
   await expect(modalEnviarTransferencia.cuentaOrigenDropdown).toBeVisible()
   await expect(modalEnviarTransferencia.cuentaOrigenOption).toBeVisible()
